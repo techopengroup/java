e7318afa8d37a4eb629268297a35fc22105f1d7d
techopengroup <techopengroup@gmail.com>
2023-07-24 22:00:09 +0530

    first commit

--- javademo/src/main/java/controller/Demo.java
+++ javademo/src/main/java/controller/Demo.java
@@ -7,6 +7,8 @@ public class Demo {
       System.out.println("JAva Git Test");
       System.out.println("JAva Git Test");
       System.out.println("JAva Git Test");
+      System.out.println("JAva Git Test");
+
 
 	}
 
