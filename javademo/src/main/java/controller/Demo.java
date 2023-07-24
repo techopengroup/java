56594a04676351e3fce8a8ca3babc0995c026b28
techopengroup <techopengroup@gmail.com>
2023-07-24 15:16:28 +0530

    second commit

--- javademo/src/main/java/controller/Demo.java
+++ javademo/src/main/java/controller/Demo.java
@@ -5,6 +5,9 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("JAva Git Test");
+      System.out.println("JAva Git Test");
+      System.out.println("JAva Git Test");
+
 	}
 
 }
